75323fab817c193bf85f166e419e75a5fe8f53f7
danyal shah <danyalshah9009@gmail.com>
2024-12-19 21:16:14 +0500

    remove item

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
     console.log(matchingProduct)
         cartSummaryHTML+=
         `
-<div class="cart-item-container">
+<div class="cart-item-container js-cart-item-container-${productId}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -106,7 +106,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
 link.addEventListener('click',()=>{
 const productId = link.dataset.productId
 remove_cart(productId);
-console.log(cart)
+const container = document.querySelector(`.js-cart-item-container-${productId}`)
+container.remove()
 })
 
 })
