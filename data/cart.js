2447e11abed4273dbe370bb435de5887b7c9c9e8
danyal shah <danyalshah9009@gmail.com>
2024-12-19 21:23:32 +0500

    save to local storage

--- data/cart.js
+++ data/cart.js
@@ -1,17 +1,30 @@
-export let cart = [
-    {
-        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2,
-    },
-    {
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1
-    },
-    {
-        productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-        quantity: 4
-    }
-]
+export let cart = JSON.parse(localStorage.getItem('cart'))
+
+if(!cart){
+    cart = 
+    [
+        {
+            productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2,
+        },
+        {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1
+        },
+        {
+            productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+            quantity: 4
+        }
+    ]
+    
+
+}
+
+
+function saveLocal() {
+    localStorage.setItem('cart',JSON.stringify(cart))
+    
+}
 export function add_to_cart(productId) {
     let matchingItem;
     cart.forEach((cartItem) => {
@@ -30,6 +43,7 @@ export function add_to_cart(productId) {
             quantity: 1
         })
     }
+    saveLocal();
 }
 
 
@@ -44,5 +58,5 @@ export function remove_cart(productId){
          
     })
     cart = newCart;
-
+    saveLocal();
 }
